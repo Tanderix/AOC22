7a87b6576e3ffcb8ff828161a65bd86ceafb5926
Andrea Carozzi <carozzi.andrea@gmail.com>
2022-12-04 11:01:04 +0100

    Create day04.py

---
+++ Day04/day04.py
@@ -0,0 +1,26 @@
+def toList(assignment):
+    start, finish = assignment.split("-")
+    return [x for x in range(int(start), int(finish)+1)]
+
+
+f = open("d4input.txt", "r")
+contained = 0
+overlap = 0
+for asspair in f:
+    asspair = asspair.strip("\n")
+    e1, e2 = asspair.split(",")
+    l1 = (toList(e1))
+    l2 = (toList(e2))
+
+    # Part 1
+    if set(l1) & set(l2) != set():
+        overlap += 1
+
+    # Part 2
+    if set(l1) & set(l2) == set(l2) or set(l1) & set(l2) == set(l1):
+        contained += 1
+
+print("Part 1: " + str(contained))
+print("Part 2: " + str(overlap))
+
+
